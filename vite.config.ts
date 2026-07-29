45451dfe921d1971de29c4b1d229ed616fc710bc
pistunchik <43292438+pistunchik@users.noreply.github.com>
2026-07-29 18:27:26 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig(() => {
       port: 3000,
       proxy: {
         '/api': {
-          target: 'http://localhost:3001',
+          target: 'http://94.102.89.181:3001',
           changeOrigin: true,
         },
       },
